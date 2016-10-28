5fb27361804f7c57e8a35ecb9eb6524ad6532d90
Bryan Fillmer <bfillmer@fanatics.com>
2016-10-28 14:06:41 -0600

    Added test runner and a boilerplate test template.

--- app/state/utils.js
+++ app/state/utils.js
@@ -5,19 +5,6 @@ import { connect } from 'react-redux'
 export const assign = Object.assign
 
 /**
- * Map reducers to their action types. Alternative to using a switch statement,
- * still returns the default state if nothing matches.
- * @param  {object} initialState Initial state for this reducer tree.
- * @param  {object} reducerMap   Map of action types to reducer functions.
- * @return {object}              New state after reducer is run.
- */
-export const mapReducers = (initialState, reducerMap) => (state = initialState, action) => {
-  const reducer = reducerMap[action.type]
-  if (typeof reducer === 'undefined') return state
-  return reducer(state, action)
-}
-
-/**
  * Connect a given Container Component to the store with or without
  * mapStateToProps or mapDispatchToProps.
  * @param  {object} store            Redux store
---
+++ app/test.tpl.js
@@ -0,0 +1,19 @@
+
+/**
+ * Test Template
+ * Copy and name thus: *.test.js, mapping the filename to match
+ * the component you wish to test.
+ */
+
+import test from 'tape'
+
+// For each unit test you write, answer these questions:
+test('What component aspect are you testing?', assert => {
+  const actual = 'What is the actual output?'
+  const expected = 'What is the expected output?'
+
+  assert.equal(actual, expected,
+    'What should the feature do?')
+
+  assert.end()
+})
